df1157f6a58365faee031c647abd6e678845d394
RLVasquez <rlvc2112000@hotmail.com>
2023-03-09 12:16:15 -0500

    Cambiando funcionalidades en botones por arreglos version ALFA

---
+++ scripts.js
@@ -0,0 +1,25 @@
+var operacionA;
+var operacionB;
+var operacion;
+
+function init (){
+    var resultado = document.getElementById("resultado");
+    var cero = document.getElementById("cero");
+    var uno = document.getElementById("uno");
+    var dos = document.getElementById("dos");
+    var tres = document.getElementById("tres");
+    var cuatro = document.getElementById("cuatro");
+    var cinco = document.getElementById("cinco");
+    var seis = document.getElementById("seis");
+    var siete = document.getElementById("siete");
+    var ocho = document.getElementById("ocho");
+    var nueve = document.getElementById("nueve");
+    var igual = document.getElementById("igual");
+    var decimal = document.getElementById("decimal");
+    var suma = document.getElementById("suma");
+    var resta = document.getElementById("resta");
+    var multi = document.getElementById("multi");
+    var divi = document.getElementById("divi");
+
+    
+}
