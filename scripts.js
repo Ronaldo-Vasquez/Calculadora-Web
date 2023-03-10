5efe4f52e9c6eb9dc3d113cf027f79d89821f9ad
RLuisVasquez <rlvc2112000@hotmail.com>
2023-03-10 12:39:50 -0500

    prueba push remoto

--- scripts.js
+++ scripts.js
@@ -1,9 +1,11 @@
 var operacionA;
 var operacionB;
 var operacion;
+let opDisplay = [];
+let start = false;//para indicar que se esta iniciando una nueva operacion matematica.
 
 function init (){
-    var resultado = document.getElementById("resultado");
+    var display = document.getElementById("dispaly");//
     var cero = document.getElementById("cero");
     var uno = document.getElementById("uno");
     var dos = document.getElementById("dos");
@@ -20,6 +22,135 @@ function init (){
     var resta = document.getElementById("resta");
     var multi = document.getElementById("multi");
     var divi = document.getElementById("divi");
+    var potencia = document.getElementById("potencia");
+    var limpiar = document.getElementById("limpiar");
+    var borrar = document.getElementById("borrar");
 
+    cero.onclick = function(){
+        opDisplay.push("0");
+       updateDisplay();
+    }
+    uno.onclick = function(){
+        opDisplay.push("1");
+       updateDisplay();
+    }
+    dos.onclick = function(){
+        opDisplay.push("2");
+       updateDisplay();
+    }
+    tres.onclick = function(){
+        opDisplay.push("3");
+       updateDisplay();
+    }
+    cuatro.onclick = function(){
+        opDisplay.push("4");
+       updateDisplay();
+    }
+    cinco.onclick = function(){
+        opDisplay.push("5");
+       updateDisplay();
+    }
+    seis.onclick = function(){
+        opDisplay.push("6");
+       updateDisplay();
+    }
+    siete.onclick = function(){
+        opDisplay.push("7");
+       updateDisplay();
+    }
+    ocho.onclick = function(){
+        opDisplay.push("8");
+       updateDisplay();
+    }
+    nueve.onclick = function(){
+        opDisplay.push("9");
+       updateDisplay();
+    }
+    suma.onclick = function(){
+        opDisplay.push("9");
+       updateDisplay();
+    }
+    resta.onclick = function(){
+        opDisplay.push("9");
+       updateDisplay();
+    }
+    multi.onclick = function(){
+        opDisplay.push("9");
+       updateDisplay();
+    }
+    divi.onclick = function(){
+        opDisplay.push("9");
+       updateDisplay();
+    }
+    limpiar.onclick = function(){
+        opDisplay = [""];
+        start = true;
+        updateDisplay();
+        
+        
+    }
     
+    
+
+
+
+}
+
+function updateDisplay(){//actualiza el la vista del ususario
+    display.textContent = opDisplay.join("");
+    
+    
+    if (start === true) {//para controlar el cero al borrar la info
+        display.textContent = "0";
+        start = false;
+    }
+}
+
+
+function updateDisplay(){//actualiza el la vista del ususario
+    display.textContent = opDisplay.join("");
+    
+    
+    if (start === true) {//para controlar el cero al borrar la info
+        display.textContent = "0";
+        start = false;
+    }
+}
+
+function updateDisplay(){//actualiza el la vista del ususario
+    display.textContent = opDisplay.join("");
+    
+    
+    if (start === true) {//para controlar el cero al borrar la info
+        display.textContent = "0";
+        start = false;
+    }
+}
+
+function updateDisplay(){//actualiza el la vista del ususario
+    display.textContent = opDisplay.join("");
+    
+    
+    if (start === true) {//para controlar el cero al borrar la info
+        display.textContent = "0";
+        start = false;
+    }
+}
+function updateDisplay(){//actualiza el la vista del ususario
+    display.textContent = opDisplay.join("");
+    
+    
+    if (start === true) {//para controlar el cero al borrar la info
+        display.textContent = "0";
+        start = false;
+    }
+}
+function updateDisplay(){//actualiza el la vista del ususario
+    display.textContent = opDisplay.join("");
+    
+    
+    if (start === true) {//para controlar el cero al borrar la info
+        display.textContent = "0";
+        start = false;
+    }
 }
